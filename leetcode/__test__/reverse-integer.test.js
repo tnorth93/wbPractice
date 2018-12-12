c5e5bb190fed9bff6db28768e1a15e841e5cedaa
Tom <tnorth93@gmail.com>
2018-12-11 20:22:07 -0800

    first test for reverse-integer completed

--- leetcode/__test__/reverse-integer.test.js
+++ leetcode/__test__/reverse-integer.test.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const reverseInteger = require('../reverse-integer/reverse-integer');
+
+describe('#reverse-integer.js', () => {
+  test('return 0 if number given is greater than 32-bit signed integer', () => {
+    expect(reverseInteger(11111111111111111)).toEqual(0);
+  });
+
+  test('return correct all being equal', () => {
+    let correctAns = romanToInteger('III');
+    expect(correctAns).toEqual(3);
+  });
+
+  test('return correct with all addition', () => {
+    let correctAns = romanToInteger('CLVI');
+    expect(correctAns).toEqual(156);
+  });
+});
